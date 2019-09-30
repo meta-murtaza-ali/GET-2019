24dba434793fe188e3ee5dff6e9f455d7d8ab53d
meta-murtaza-ali <52956136+meta-murtaza-ali@users.noreply.github.com>
2019-09-30 16:21:01 +0530

    Update SpringSecurityJdbcApplicationTests.java

--- EAD/EAD-14/src/test/java/com/metacube/SpringSecurityJDBC/SpringSecurityJdbcApplicationTests.java
+++ EAD/EAD-14/src/test/java/com/metacube/SpringSecurityJDBC/SpringSecurityJdbcApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 /**
  * 
- * @author Pankaj
+ * @author Murtaza Ali
  *
  */
 @RunWith(SpringRunner.class)
